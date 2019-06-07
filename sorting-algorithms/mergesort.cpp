186aa7927171717517d3a913e3c452c271baac57
abhinavsingh23 <abhinavsingh2902@gmail.com>
2019-06-07 12:29:57 +0530

    corrections

--- sorting-algorithms/mergesort.cpp
+++ sorting-algorithms/mergesort.cpp
@@ -3,15 +3,15 @@ using namespace std;
 #include <iostream>
 #include<stdio.h>
 
-void merge(int *Arr_l,int *Arr_r, int *Arr){
+void merge(int *Arr_l,int *Arr_r, int *Arr, int nl,int nr,int n){
 	
+
 	int i,j,k;
 	i=0;
 	j=0;
 	k=0;
-	int nl = sizeof(Arr_l)/sizeof(Arr_l[0]);
-	int nr = sizeof(Arr_r)/sizeof(Arr_r[0]);
-	int n = sizeof(Arr)/sizeof(Arr[0]);
+	
+
 	while(i<nl && j<nr){
 		if(Arr_l[i]<Arr_r[j]){
 			Arr[k]=Arr_l[i];
@@ -23,13 +23,24 @@ void merge(int *Arr_l,int *Arr_r, int *Arr){
 			j=j+1;
 			k=k+1;
 		}
+		if(Arr_l[i]==Arr_r[j]){
+			Arr[k]=Arr_r[j];
+			Arr[k+1]=Arr_l[i];
+			i++;
+			j++;
+			k=k+2;
+		}
 	}
 
 	while(i<nl){
 		Arr[k]=Arr_l[i];
+		i++;		
+		k++;
 	}
 	while(j<nl){
 		Arr[k]=Arr_r[j];
+		j++;
+		k++;
 	}
 
 }
@@ -39,6 +50,7 @@ void mergesort(int Arr[] ,int n){
 
 	int i;
 	if(n<2){
+
 		return ;
 	}
 	
@@ -46,17 +58,18 @@ void mergesort(int Arr[] ,int n){
 	int nr= n-nl;
 	int Arr_r[nr];
 	int Arr_l[nl];
-	cout << nl<<endl;
-	
+
+
 	for(i=0;i<nl;i++){
 		Arr_l[i]=Arr[i];
 	}
+
 	for(i=0;i<nr;i++){
 		Arr_r[i]=Arr[nl+i];
 	}
 	mergesort(Arr_l,nl);
 	mergesort(Arr_r,nr);
-	merge(Arr_l,Arr_r,Arr);
+	merge(Arr_l,Arr_r,Arr,nl,nr,n);
 
 	
 	
@@ -76,10 +89,11 @@ int main(){
 	}
 	
 	mergesort(Arr,n);
+
 	for(i=0;i<n;i++){
-		cout << Arr[i] << " " << endl;
+		cout << Arr[i] << " ";
 	}
-	
+	cout << endl;
 	return 0;	
 
 }
